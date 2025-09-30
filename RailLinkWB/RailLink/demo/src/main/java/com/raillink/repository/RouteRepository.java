ade86a275bf8af0ce1131237254d57419fa52ba8
AhamedAAHA <hubaibahamedaaha@gmail.com>
2025-09-30 13:54:18 +0530

    route

---
+++ RailLinkWB/RailLink/demo/src/main/java/com/raillink/repository/RouteRepository.java
@@ -0,0 +1,13 @@
+package com.raillink.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.raillink.model.Route;
+
+@Repository
+public interface RouteRepository extends JpaRepository<Route, Long> {
+    Optional<Route> findByName(String name);
+} 
